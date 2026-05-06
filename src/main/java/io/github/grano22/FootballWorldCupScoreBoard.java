5fd5a79e00fa0424f21b0d4c896c601a130c32ad
Adrian Błasiak <grano22@outlook.com>
2026-05-06 13:01:40 +0200

    feat(game-board-logic): Present multiple started games with most recently added order

--- src/main/java/io/github/grano22/FootballWorldCupScoreBoard.java
+++ src/main/java/io/github/grano22/FootballWorldCupScoreBoard.java
@@ -1,12 +1,9 @@
 package io.github.grano22;
 
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
 
 public final class FootballWorldCupScoreBoard {
-    private final Map<Map.Entry<String, String>, Map.Entry<Integer, Integer>> games = new HashMap<>();
+    private final LinkedHashMap<Map.Entry<String, String>, Map.Entry<Integer, Integer>> games = new LinkedHashMap<>();
 
     public void startGame(String homeTeamName, String awayTeamName) {
         games.put(
@@ -21,7 +18,7 @@ public final class FootballWorldCupScoreBoard {
         }
 
         final var summary = new StringJoiner("\n");
-        for (var game : games.entrySet()) {
+        for (var game : games.reversed().entrySet()) {
             final var matchScore = new StringBuilder();
             matchScore
                 .append(game.getKey().getKey()).append(' ').append(game.getValue().getKey()).append(" - ")
--- src/test/java/io/github/grano22/FootballWorldCupScoreBoardTest.java
+++ src/test/java/io/github/grano22/FootballWorldCupScoreBoardTest.java
@@ -2,6 +2,9 @@ package io.github.grano22;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FootballWorldCupScoreBoardTest {
@@ -28,5 +31,37 @@ public class FootballWorldCupScoreBoardTest {
                 Mexico 0 - Canada 0""",
                 footballWorldCupScoreBoard.getASummaryOfGamesByTotalScore()
         );
+    }
+
+    @Test
+    public void scoreBoardHasShowedRegisteredInitialMatchesOrderedByMostRecentlyAddedEntries() {
+        // Arrange
+        var footballWorldCupScoreBoard = new FootballWorldCupScoreBoard();
+
+        // Act
+        startGames(footballWorldCupScoreBoard, List.of(
+            Map.entry("Mexico", "Canada"),
+            Map.entry("Spain", "Brazil"),
+            Map.entry("Germany", "France"),
+            Map.entry("Uruguay", "Italy"),
+            Map.entry("Argentina", "Australia")
+        ));
+
+        // Assert
+        assertEquals(
+                """
+                Argentina 0 - Australia 0
+                Uruguay 0 - Italy 0
+                Germany 0 - France 0
+                Spain 0 - Brazil 0
+                Mexico 0 - Canada 0""",
+                footballWorldCupScoreBoard.getASummaryOfGamesByTotalScore()
+        );
+    }
+
+    private void startGames(FootballWorldCupScoreBoard gameBoard, List<Map.Entry<String, String>> gamesToStart) {
+        for (final var game : gamesToStart) {
+            gameBoard.startGame(game.getKey(), game.getValue());
+        }
     }
 }
